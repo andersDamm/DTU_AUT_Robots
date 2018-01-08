378146ef450b74dd7d3dc813611d1adc2b0250a1
Unknown <s163933@student.dtu.dk>
2018-01-08 16:14:48 +0100

    Cleanup and more comments

--- square_AutoStraight.c
+++ square_AutoStraight.c
@@ -35,14 +35,6 @@ void xml_proc(struct xml_in *x);
 void xml_proca(struct xml_in *x);
 
 typedef struct { int time;
-	double motorspeed_l, motorspeed_r;
-} speedTimeLog_t;
-
-/*
-* Went from ex3.1 to 3.2
-* Removed the output of the speed to a file, introduced keeping track of x, y and theta.
-*/
-typedef struct { int time;
 	double x,y,theta;
 } poseTimeLog_t;
 
@@ -50,24 +42,24 @@ typedef struct { int time;
 componentservertype lmssrv,camsrv;
 
 symTableElement *
-    getinputref (const char *sym_name, symTableElement * tab)
-    {
-      int i;
-      for (i=0; i< getSymbolTableSize('r'); i++)
-	if (strcmp (tab[i].name,sym_name) == 0)
-	  return &tab[i];
-      return 0;
-    }
+getinputref (const char *sym_name, symTableElement * tab)
+{
+  int i;
+  for (i=0; i< getSymbolTableSize('r'); i++)
+   if (strcmp (tab[i].name,sym_name) == 0)
+     return &tab[i];
+   return 0;
+ }
 
-    symTableElement *
-    getoutputref (const char *sym_name, symTableElement * tab)
-    {
-      int i;
-      for (i=0; i< getSymbolTableSize('w'); i++)
-	if (strcmp (tab[i].name,sym_name) == 0)
-	  return &tab[i];
-      return 0;
-    }
+ symTableElement *
+ getoutputref (const char *sym_name, symTableElement * tab)
+ {
+  int i;
+  for (i=0; i< getSymbolTableSize('w'); i++)
+   if (strcmp (tab[i].name,sym_name) == 0)
+     return &tab[i];
+   return 0;
+ }
 /*****************************************
 * odometry
 */
@@ -78,8 +70,8 @@ symTableElement *
 #define AJAX 0.5 /* m/s² */
 #define SIZE_OF_ARRAY 10000
 #define CONVERSION_FACTOR_ACC 0.01
-#define K 1
-#define K_ACC 0.001
+#define K_FOR_STRAIGHT_DIRECTION_CONTROL 1
+#define K_FOR_ACCELERATING_DIRECTION_CONTROL 0.001
 
 typedef struct{ //input signals
 		int left_enc,right_enc; // encoderticks
@@ -87,14 +79,14 @@ typedef struct{ //input signals
 		double w;	// wheel separation
 		double cr,cl;   // meters per encodertick
 		//output signals
-		double right_pos,left_pos, ypos, xpos, theta, theta_ref;
+		double right_pos,left_pos, y_pos, x_pos, theta, theta_ref;
 		// internal variables
 		int left_enc_old, right_enc_old;
-		} odotype;
+  } odotype;
 
-void reset_odo(odotype *p);
-void update_odo(odotype *p);
-
+  void reset_odo(odotype *p);
+  void update_odo(odotype *p);
+  int log_data_to_file(poseTimeLog_t * poseTimeLog_out, int size);
 
 
 
@@ -103,21 +95,21 @@ void update_odo(odotype *p);
 */
 
 typedef struct{//input
-		int cmd;
-		int curcmd;
-		double speedcmd;
-		double dist;
-		double angle;
-		double start_angle;
-		double left_pos,right_pos;
+  int cmd;
+  int curcmd;
+  double speedcmd;
+  double dist;
+  double angle;
+  double start_angle;
+  double left_pos,right_pos;
 		// parameters
-		double w;
+  double w;
 		//output
-		double motorspeed_l,motorspeed_r;
-		int finished;
+  double motorspeed_l,motorspeed_r;
+  int finished;
 		// internal variables
-		double startpos;
-	      }motiontype;
+  double startpos;
+}motiontype;
 
 enum {mot_stop=1,mot_move,mot_turn};
 
@@ -130,9 +122,9 @@ int turn(double angle, double speed,int time);
 
 
 typedef struct{
-		int state,oldstate;
-		int time;
-	      }smtype;
+  int state,oldstate;
+  int time;
+}smtype;
 
 void sm_update(smtype *p);
 
@@ -152,55 +144,54 @@ int main()
   poseTimeLog_t poseTimeLog_a[SIZE_OF_ARRAY];
   int counter = 0;
 
-  FILE *outFile;
   int i;
   int running,n=0,arg,time=0;
   double dist=0,angle=0;
 
   /* Establish connection to robot sensors and actuators.
   */
-    if (rhdConnect('w',"localhost",ROBOTPORT)!='w'){
-	printf("Can't connect to rhd \n");
-	exit(EXIT_FAILURE);
-      }
+  if (rhdConnect('w',"localhost",ROBOTPORT)!='w'){
+   printf("Can't connect to rhd \n");
+   exit(EXIT_FAILURE);
+ }
 
-      printf("connected to robot \n");
-      if ((inputtable=getSymbolTable('r'))== NULL){
-	printf("Can't connect to rhd \n");
-	exit(EXIT_FAILURE);
-      }
-      if ((outputtable=getSymbolTable('w'))== NULL){
-	printf("Can't connect to rhd \n");
-	exit(EXIT_FAILURE);
-      }
+ printf("connected to robot \n");
+ if ((inputtable=getSymbolTable('r'))== NULL){
+   printf("Can't connect to rhd \n");
+   exit(EXIT_FAILURE);
+ }
+ if ((outputtable=getSymbolTable('w'))== NULL){
+   printf("Can't connect to rhd \n");
+   exit(EXIT_FAILURE);
+ }
       // connect to robot I/O variables
-      lenc=getinputref("encl",inputtable);
-      renc=getinputref("encr",inputtable);
-      linesensor=getinputref("linesensor",inputtable);
-      irsensor=getinputref("irsensor",inputtable);
+ lenc=getinputref("encl",inputtable);
+ renc=getinputref("encr",inputtable);
+ linesensor=getinputref("linesensor",inputtable);
+ irsensor=getinputref("irsensor",inputtable);
 
-      speedl=getoutputref("speedl",outputtable);
-      speedr=getoutputref("speedr",outputtable);
-      resetmotorr=getoutputref("resetmotorr",outputtable);
-      resetmotorl=getoutputref("resetmotorl",outputtable);
+ speedl=getoutputref("speedl",outputtable);
+ speedr=getoutputref("speedr",outputtable);
+ resetmotorr=getoutputref("resetmotorr",outputtable);
+ resetmotorl=getoutputref("resetmotorl",outputtable);
     // **************************************************
 //  Camera server code initialization
 //
 
 /* Create endpoint */
-  lmssrv.port=24919;
-  strcpy(lmssrv.host,"127.0.0.1");
-  strcpy(lmssrv.name,"laserserver");
-  lmssrv.status=1;
-  camsrv.port=24920;
-  strcpy(camsrv.host,"127.0.0.1");
-  camsrv.config=1;
-  strcpy(camsrv.name,"cameraserver");
-  camsrv.status=1;
+ lmssrv.port=24919;
+ strcpy(lmssrv.host,"127.0.0.1");
+ strcpy(lmssrv.name,"laserserver");
+ lmssrv.status=1;
+ camsrv.port=24920;
+ strcpy(camsrv.host,"127.0.0.1");
+ camsrv.config=1;
+ strcpy(camsrv.name,"cameraserver");
+ camsrv.status=1;
 
-  if (camsrv.config) {
-      int errno = 0;
-      camsrv.sockfd = socket(AF_INET, SOCK_STREAM, 0);
+ if (camsrv.config) {
+  int errno = 0;
+  camsrv.sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if ( camsrv.sockfd < 0 )
   {
     perror(strerror(errno));
@@ -223,11 +214,11 @@ int main()
 //
 
 /* Create endpoint */
-  lmssrv.config=1;
-  if (lmssrv.config) {
-      char buf[256];
-      int errno = 0,len;
-      lmssrv.sockfd = socket(AF_INET, SOCK_STREAM, 0);
+lmssrv.config=1;
+if (lmssrv.config) {
+  char buf[256];
+  int errno = 0,len;
+  lmssrv.sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if ( lmssrv.sockfd < 0 )
   {
     perror(strerror(errno));
@@ -246,119 +237,113 @@ int main()
 }
 
 
-  /* Read sensors and zero our position.
+  /* 
+  /  Read sensors and zero our position.
   */
-  rhdSync();
+rhdSync();
 
-  odo.w=0.256;
-  odo.cr=DELTA_M;
-  odo.cl=odo.cr;
-  odo.left_enc=lenc->data[0];
-  odo.right_enc=renc->data[0];
-  reset_odo(&odo);
-  printf("position: %f, %f\n", odo.left_pos, odo.right_pos);
-  mot.w=odo.w;
+odo.w=0.256;
+odo.cr=DELTA_M;
+odo.cl=odo.cr;
+odo.left_enc=lenc->data[0];
+odo.right_enc=renc->data[0];
+reset_odo(&odo);
+printf("position: %f, %f\n", odo.left_pos, odo.right_pos);
+mot.w=odo.w;
 running=1;
 mission.state=ms_init;
 mission.oldstate=-1;
 while (running){
   if (lmssrv.config && lmssrv.status && lmssrv.connected){
-	  while ( (xml_in_fd(xmllaser,lmssrv.sockfd) >0))
-	    xml_proca(xmllaser);
-      }
+   while ( (xml_in_fd(xmllaser,lmssrv.sockfd) >0))
+     xml_proca(xmllaser);
+ }
 
-      if (camsrv.config && camsrv.status && camsrv.connected){
-	  while ( (xml_in_fd(xmldata,camsrv.sockfd) >0))
-	    xml_proc(xmldata);
-      }
+ if (camsrv.config && camsrv.status && camsrv.connected){
+   while ( (xml_in_fd(xmldata,camsrv.sockfd) >0))
+     xml_proc(xmldata);
+ }
 
 
-  rhdSync();
-  odo.left_enc=lenc->data[0];
-  odo.right_enc=renc->data[0];
-  update_odo(&odo);
+ rhdSync();
+ odo.left_enc=lenc->data[0];
+ odo.right_enc=renc->data[0];
+ update_odo(&odo);
 
 /****************************************
 / mission statemachine
 */
-  sm_update(&mission);
-  switch (mission.state) {
-    case ms_init:
-      n=4; dist=2;angle= - 90.0/180*M_PI;
-      mission.state= ms_fwd;
-    break;
+ sm_update(&mission);
+ switch (mission.state) {
+  case ms_init:
+  n=4; dist=2;angle= - 90.0/180*M_PI;
+  mission.state= ms_fwd;
+  break;
 
-    case ms_fwd:
+  case ms_fwd:
       if (fwd(dist,0.6,mission.time))  mission.state=ms_turn; // Square min ven
-    break;
+      break;
 
-    case ms_turn:
+      case ms_turn:
       if (turn(angle,0.3,mission.time)){
-	n=n-1;
-	if (n==0)
-	  mission.state=ms_end;
-	else
-	  mission.state=ms_fwd;
-	}
-    break;
+       n=n-1;
+       if (n==0)
+         mission.state=ms_end;
+       else
+         mission.state=ms_fwd;
+     }
+     break;
 
-    case ms_end:
-      mot.cmd=mot_stop;
-      running=0;
+     case ms_end:
+     mot.cmd=mot_stop;
+     running=0;
       // Output the data
-      outFile = fopen("Log.dat", "w");
-      if (outFile == NULL) {
-	    fprintf(stderr, "ERROR: couldn't open %s for writing\n", "poseData_finiteAcc.dat");
-	exit(1);
-      }
-      for(i = 0;i < counter; i++) {
-		  fprintf(outFile,"%i,%f,%f,%f\n",poseTimeLog_a[i].time, poseTimeLog_a[i].x, poseTimeLog_a[i].y, poseTimeLog_a[i].theta);
-      }
-      fclose(outFile);
+     if(log_data_to_file(poseTimeLog_a, counter)){
+      printf("No data logged\n");
+    }
     break;
   }
 
   /**********************************
     * Log the data
     */
-    if (counter<SIZE_OF_ARRAY) {
-	poseTimeLog_a[counter].time=counter;
-	poseTimeLog_a[counter].x = odo.xpos;
-	poseTimeLog_a[counter].y = odo.ypos;
-	poseTimeLog_a[counter].theta = odo.theta;
-	counter++;
-      } else{
-	printf("array out of bounds");
-      }
-
-printf("odo.theta: %f \t odo.theta_ref: %f\n", odo.theta, odo.theta_ref);
+  if (counter<SIZE_OF_ARRAY) {
+   poseTimeLog_a[counter].time=counter;
+   poseTimeLog_a[counter].x = odo.x_pos;
+   poseTimeLog_a[counter].y = odo.y_pos;
+   poseTimeLog_a[counter].theta = odo.theta;
+   counter++;
+ } else{
+   printf("array out of bounds\n");
+ }
 
 /*  end of mission  */
 
-  mot.left_pos=odo.left_pos;
-  mot.right_pos=odo.right_pos;
-  update_motcon(&mot);
-  speedl->data[0]=100*mot.motorspeed_l;
-  speedl->updated=1;
-  speedr->data[0]=100*mot.motorspeed_r;
-  speedr->updated=1;
-  if (time  % 100 ==0)
+ mot.left_pos=odo.left_pos;
+ mot.right_pos=odo.right_pos;
+ update_motcon(&mot);
+ speedl->data[0]=100*mot.motorspeed_l;
+ speedl->updated=1;
+ speedr->data[0]=100*mot.motorspeed_r;
+ speedr->updated=1;
+ if (time  % 100 ==0)
+    //    Good place for outputting debugging variables.
     //    printf(" laser %f \n",laserpar[3]);
   time++;
 /* stop if keyboard is activated
 *
 */
-  ioctl(0, FIONREAD, &arg);
-  if (arg!=0)  running=0;
+ioctl(0, FIONREAD, &arg);
+if (arg!=0)  running=0;
 
 }/* end of main control loop */
-  speedl->data[0]=0;
-  speedl->updated=1;
-  speedr->data[0]=0;
-  speedr->updated=1;
-  rhdSync();
-  rhdDisconnect();
-  exit(0);
+speedl->data[0]=0;
+speedl->updated=1;
+speedr->data[0]=0;
+speedr->updated=1;
+rhdSync();
+rhdDisconnect();
+exit(0);
 }
 
 
@@ -371,7 +356,7 @@ printf("odo.theta: %f \t odo.theta_ref: %f\n", odo.theta, odo.theta_ref);
 
 void reset_odo(odotype * p)
 {
-  p->right_pos = p->left_pos = p->xpos = p->ypos = p->theta = 0.0;
+  p->right_pos = p->left_pos = p->x_pos = p->y_pos = p->theta = 0.0;
   p->right_enc_old = p->right_enc;
   p->left_enc_old = p->left_enc;
   p->theta_ref = 0;
@@ -397,8 +382,8 @@ void update_odo(odotype *p)
   delta_u = ((double)delta_r * p->cr + (double)delta_l * p->cl)/2;
   delta_theta = ((double)delta_r * p->cr - (double)delta_l * p->cl)/(double)p->w;
   p->theta += delta_theta;
-  p->xpos += delta_u*cos(p->theta);
-  p->ypos -= delta_u*sin(p->theta);
+  p->x_pos += delta_u*cos(p->theta);
+  p->y_pos -= delta_u*sin(p->theta);
 
 }
 
@@ -407,11 +392,11 @@ void update_motcon(motiontype *p){
 
   double d, d_angle;
 
-if (p->cmd !=0){
+  if (p->cmd !=0){
 
     p->finished=0;
     switch (p->cmd){
-    case mot_stop:
+      case mot_stop:
       p->curcmd=mot_stop;
       break;
       case mot_move:
@@ -420,93 +405,93 @@ if (p->cmd !=0){
       break;
 
       case mot_turn:
-	if (p->angle > 0)
-	    p->startpos=p->right_pos;
-	else
-	    p->startpos=p->left_pos;
-	p->curcmd=mot_turn;
-      break;
+      if (p->angle > 0)
+       p->startpos=p->right_pos;
+     else
+       p->startpos=p->left_pos;
+     p->curcmd=mot_turn;
+     break;
 
 
-    }
+   }
 
-    p->cmd=0;
+   p->cmd=0;
+ }
+
+ switch (p->curcmd){
+  case mot_stop:
+  p->motorspeed_l=0;
+  p->motorspeed_r=0;
+  break;
+
+  case mot_move:
+  d=((p->motorspeed_l+p->motorspeed_r)/2)*((p->motorspeed_l+p->motorspeed_r)/2)/(2*(AJAX));
+
+  if ((p->right_pos+p->left_pos)/2- p->startpos >= p->dist){
+   p->finished=1;
+   p->motorspeed_l=0;
+   p->motorspeed_r=0;
   }
-
-  switch (p->curcmd){
-    case mot_stop:
-      p->motorspeed_l=0;
-      p->motorspeed_r=0;
-    break;
-
-    case mot_move:
-      d=((p->motorspeed_l+p->motorspeed_r)/2)*((p->motorspeed_l+p->motorspeed_r)/2)/(2*(AJAX));
-
-      if ((p->right_pos+p->left_pos)/2- p->startpos >= p->dist){
-	p->finished=1;
-	p->motorspeed_l=0;
-	p->motorspeed_r=0;
-      }
       else if((p->right_pos+p->left_pos)/2- p->startpos > p->dist-d){ // Deacceleration
-	p->motorspeed_l -= AJAX*CONVERSION_FACTOR_ACC - K_ACC*(odo.theta_ref - odo.theta);
-	p->motorspeed_r -= AJAX*CONVERSION_FACTOR_ACC + K_ACC*(odo.theta_ref - odo.theta);
+        p->motorspeed_l -= AJAX*CONVERSION_FACTOR_ACC - K_FOR_ACCELERATING_DIRECTION_CONTROL*(odo.theta_ref - odo.theta);
+        p->motorspeed_r -= AJAX*CONVERSION_FACTOR_ACC + K_FOR_ACCELERATING_DIRECTION_CONTROL*(odo.theta_ref - odo.theta);
       }
       else if(p->motorspeed_l<p->speedcmd){                           // Acceleration
-	p->motorspeed_l += AJAX*CONVERSION_FACTOR_ACC - K_ACC*(odo.theta_ref - odo.theta);
-	p->motorspeed_r += AJAX*CONVERSION_FACTOR_ACC + K_ACC*(odo.theta_ref - odo.theta);
+        p->motorspeed_l += AJAX*CONVERSION_FACTOR_ACC - K_FOR_ACCELERATING_DIRECTION_CONTROL*(odo.theta_ref - odo.theta);
+        p->motorspeed_r += AJAX*CONVERSION_FACTOR_ACC + K_FOR_ACCELERATING_DIRECTION_CONTROL*(odo.theta_ref - odo.theta);
       }
-      else {
-	p->motorspeed_l=p->speedcmd - K*(odo.theta_ref - odo.theta);
-	p->motorspeed_r=p->speedcmd + K*(odo.theta_ref - odo.theta);
+     else {                                                           // Constant speed and tracks direction
+       p->motorspeed_l=p->speedcmd - K_FOR_STRAIGHT_DIRECTION_CONTROL*(odo.theta_ref - odo.theta);
+       p->motorspeed_r=p->speedcmd + K_FOR_STRAIGHT_DIRECTION_CONTROL*(odo.theta_ref - odo.theta);
       }
-    break;
+     break;
 
-    case mot_turn:
+     case mot_turn:
       if (p->angle>0){ 		//Turn left
-	  d_angle=p->motorspeed_r*p->motorspeed_r/(AJAX*p->w);
+       d_angle=p->motorspeed_r*p->motorspeed_r/(AJAX*p->w);
 
-	  if(p->right_pos-p->startpos >= p->angle*p->w/2){
-	    p->motorspeed_l=0;
-	    p->motorspeed_r=0;
-	    p->finished=1;
-	  }
-	  else if(p->right_pos-p->startpos > p->angle*p->w/2-d_angle*p->w/2){ 	// (odo.theta > p->start_angle + p->angle - d_angle)
-	    p->motorspeed_l += AJAX*CONVERSION_FACTOR_ACC;
-	    p->motorspeed_r -= AJAX*CONVERSION_FACTOR_ACC;
-	  }
-	  else if(p->motorspeed_r < p->speedcmd){
-	    p->motorspeed_l -= AJAX*CONVERSION_FACTOR_ACC;
-	    p->motorspeed_r += AJAX*CONVERSION_FACTOR_ACC;
-	  }
-	  else{									//(p->right_pos-p->startpos < p->angle*p->w/2)
-	    p->motorspeed_r=p->speedcmd;
-	    p->motorspeed_l=-(p->speedcmd);
-	  }
-	}
-	else { 				//Turn right
-	  d_angle=p->motorspeed_l*p->motorspeed_l/(AJAX*p->w);
+       if(p->right_pos-p->startpos >= p->angle*p->w/2){
+         p->motorspeed_l=0;
+         p->motorspeed_r=0;
+         p->finished=1;
+        }
+	  else if(p->right_pos-p->startpos > p->angle*p->w/2-d_angle*p->w/2){ 	// Deceleration
+     p->motorspeed_l += AJAX*CONVERSION_FACTOR_ACC;
+     p->motorspeed_r -= AJAX*CONVERSION_FACTOR_ACC;
+   }
+   else if(p->motorspeed_r < p->speedcmd){                                // Acceleration
+     p->motorspeed_l -= AJAX*CONVERSION_FACTOR_ACC;
+     p->motorspeed_r += AJAX*CONVERSION_FACTOR_ACC;
+   }
+	  else{                                                                // Constant turn speed
+     p->motorspeed_r=p->speedcmd;
+     p->motorspeed_l=-(p->speedcmd);
+   }
+ }
+	else { 				        //Turn right
+   d_angle=p->motorspeed_l*p->motorspeed_l/(AJAX*p->w);
 
-	  if(p->left_pos - p->startpos >= - p->angle*p->w/2){
-	    p->motorspeed_l=0;
-	    p->motorspeed_r=0;
-	    p->finished=1;
-	  }
-	  else if(p->left_pos-p->startpos > -p->angle*p->w/2-d_angle*p->w/2){
-	    p->motorspeed_r += AJAX*CONVERSION_FACTOR_ACC;
-	    p->motorspeed_l -= AJAX*CONVERSION_FACTOR_ACC;
-	  }
-	  else if(p->motorspeed_l < p->speedcmd){
-	    p->motorspeed_r -= AJAX*CONVERSION_FACTOR_ACC;
-	    p->motorspeed_l += AJAX*CONVERSION_FACTOR_ACC;
-	  }
-	  else{			//(p->left_pos-p->startpos < - p->angle*p->w/2)
-	    p->motorspeed_l=p->speedcmd;
-	    p->motorspeed_r=-(p->speedcmd);
-	  }
-	}
+   if(p->left_pos - p->startpos >= - p->angle*p->w/2){                  // Finished
+     p->motorspeed_l=0;
+     p->motorspeed_r=0;
+     p->finished=1;
+   }
+   else if(p->left_pos-p->startpos > -p->angle*p->w/2-d_angle*p->w/2){  // Deceleration
+     p->motorspeed_r += AJAX*CONVERSION_FACTOR_ACC;
+     p->motorspeed_l -= AJAX*CONVERSION_FACTOR_ACC;
+   }
+   else if(p->motorspeed_l < p->speedcmd){                              // Acceleration
+     p->motorspeed_r -= AJAX*CONVERSION_FACTOR_ACC;
+     p->motorspeed_l += AJAX*CONVERSION_FACTOR_ACC;
+   }
+	  else{                                                               // Constant turn speed
+     p->motorspeed_l=p->speedcmd;
+     p->motorspeed_r=-(p->speedcmd);
+   }
+ }
 
-    break;
-  }
+ break;
+}
 }
 
 
@@ -527,7 +512,7 @@ int turn(double angle, double speed,int time){
     mot.speedcmd=speed;
     mot.angle=angle;
     mot.start_angle=odo.theta;
-    odo.theta_ref = odo.theta_ref + angle;
+    odo.theta_ref=odo.theta_ref + angle; //Update the desired angle
     return 0;
   }
   else
@@ -537,10 +522,28 @@ int turn(double angle, double speed,int time){
 
 void sm_update(smtype *p){
   if (p->state!=p->oldstate){
-      p->time=0;
-      p->oldstate=p->state;
+    p->time=0;
+    p->oldstate=p->state;
   }
   else {
     p->time++;
   }
+}
+
+int log_data_to_file(poseTimeLog_t * poseTimeLog_out, int size){
+  // Takes array of custom log-type and the length of this, writes to the file: Log.dat
+  FILE *outFile;
+  int i;
+  printf("\nStarting logging of data\n");
+  outFile = fopen("Log.dat", "w");
+  if (outFile == NULL) {
+    fprintf(stderr, "ERROR: couldn't open %s for writing\n", "Log.dat");
+    return 1;
+  }
+  for(i = 0;i < size; i++) {
+    fprintf(outFile,"%i,%f,%f,%f\n",poseTimeLog_out[i].time, poseTimeLog_out[i].x, poseTimeLog_out[i].y, poseTimeLog_out[i].theta);
+  }
+  fclose(outFile);
+  printf("Ended logging of data\n\n");
+  return 0;
 }
